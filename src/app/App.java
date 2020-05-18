d9d4d15551c3eecf89033775dd7b96590884c272
Pedro Leoni <pedro.leoni@marconirovereto.it>
2020-05-18 11:01:17 +0200

    metodi prod cons

--- src/app/App.java
+++ src/app/App.java
@@ -1,19 +1,35 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class App {
     
-    private int[] list = new int[10]; 
-    private Lock lock = new ReentrantLock();
-    private Condition cond = lock.newCondition();
+    private static ArrayList<Integer> product = new ArrayList<>(10);
+    private static int maxSize = 10;
+    private static Lock lock = new ReentrantLock();
+    private static Condition cond = lock.newCondition();
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        
+
     }
 
-
+    private static void producer(){
+        
+        while (true) {
+            if (product.size() < maxSize) {
+                
+            } else {
+                
+            }
+        }
+    }
+    private static void consumer(){
+        
+    }
 
 }
