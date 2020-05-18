af4daa91b85b6a28b96fe89c61f8840155f2f7b6
Pedro Leoni <pedro.leoni@marconirovereto.it>
2020-05-18 10:50:53 +0200

    creazione attributi

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,19 @@
 package app;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class App {
+    
+    private int[] list = new int[10]; 
+    private Lock lock = new ReentrantLock();
+    private Condition cond = lock.newCondition();
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
     }
+
+
+
 }
