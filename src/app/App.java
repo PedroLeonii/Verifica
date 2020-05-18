e1bdb630e4c70f951e65a9f8175c2ea92c289755
Pedro Leoni <pedro.leoni@marconirovereto.it>
2020-05-18 11:28:42 +0200

    verifica finita

--- src/app/App.java
+++ src/app/App.java
@@ -7,22 +7,54 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class App {
-    
+
     private static ArrayList<Integer> product = new ArrayList<>(10);
     private static int maxSize = 10;
     private static Lock lock = new ReentrantLock();
     private static Condition cond = lock.newCondition();
 
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        Thread th1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                try {
+                    producer();
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+
+        });
+        Thread th2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                try {
+                    consumer();
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+               
+			}
+
+        });
+
+        th1.start();
+        th2.start();
 
     }
 
-    private static void producer(){
+    private static void producer() throws InterruptedException {
         Random random = new Random();
-       try {
-           lock.lock();
-
+       
+        lock.lock();
+        try{
             while (true) {
                 if (product.size() < maxSize) {
 
@@ -30,19 +62,41 @@ public class App {
             
                 } else {
                     System.out.println("magazzino pieno....");
+                    
+                    cond.signal();
                     cond.await();
+                    
                 }
+            }
         }
-       } catch (Exception e) {
-           System.out.println(e);
-       } 
-       finally{
+        finally{
             lock.unlock();
-       }
-        
+        }
+       
     }
-    private static void consumer(){
-        
+    private static void consumer() throws InterruptedException {
+
+       
+        lock.lock();
+        try{
+            while (true) {
+                if (product.size() > 0) {
+
+                    product.remove(product.size()-1);
+                
+                } else {
+                    System.out.println("magazzino vuoto....");
+                    
+                    cond.signal();
+                    cond.await();
+   
+                }
+            }
+        }
+        finally{
+            lock.unlock();
+        }
+
     }
 
 }
